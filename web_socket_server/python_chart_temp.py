60b4a219b125730ab09c41f59b1ca121ba05ac19
rakib045 <rakib045@gmail.com>
2021-07-22 20:17:47 -0600

    Upload Text, py and ipynb file

--- web_socket_server/python_chart_temp.py
+++ web_socket_server/python_chart_temp.py
@@ -1,22 +1,17 @@
 from util_library import *
-animation_info = {
-       'title': 'My Animation',
-       'position': 'bottomright',
-}
-addAnimationToGeoChart('Chart1', 2008, 2013, animation_info)
+import numpy as np
+def square(x):
+    return x * x
 
-layer_name = 'Scalar SWE'
-variable_name = 'scalarSWE'
-color_list = ['#ffffe5', '#f7fcb9', '#d9f0a3', '#addd8e', '#82d183', '#78c679', '#41ab5d', '#238443', '#006837', '#004529']
-addColorListToGeoChart('Chart1', layer_name, color_list)
+x = np.random.randint(1, 10)
+y = square(x)
+print('%d squared is %d' % (x, y))
 
-legend_info = {
-'legend_title': 'Scalar SWE (kg m-2)', 
-'legend_position': 'bottomleft',
-'scale': 'logarithmic'
-}
-addLegendToGeoChart('Chart1', layer_name, legend_info)
-drawGeoChart('Chart1', layer_name, 'jeoJSONs/bow_river_catchment.json', variable_name, 'HRU_ID', 'average')
+
+
+
+
+
 
 result_array = sorted(result_array, key=lambda x: x['order'], reverse=False)
 print(';##;')
