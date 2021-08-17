63a618f65931c1c6491e953400e893bc44f30798
rakib045 <rakib045@gmail.com>
2021-08-17 00:41:57 -0600

    - User_id functionality in PI, socket server and GeoMap - Show Variables jsTree

--- web_socket_server/python_chart_temp.py
+++ web_socket_server/python_chart_temp.py
@@ -19,15 +19,15 @@ animation_info = {
 addAnimationToGeoChart('Chart1', 2008, 2013, animation_info)
 
 # Define layer name and corresponding variable
-layer_name = 'Average KWTroutedRunoff'
-variable_name = 'KWTroutedRunoff'
+layer_name = 'Scalar SWE'
+variable_name = 'scalarSWE'
 
 # Define Color List for a specific layer of the Geo map with chart name
 # function : addColorListToGeoChart
 # param 1 : chart_name - string - name of the chart
 # param 2 : layer_name - string - name of the layer
 # param 3 : color_list - array of string - list of all colors
-color_list = ['#ffffcc', '#ffeda0', '#fed976', '#feb24c', '#fd8d3c', '#fc5735', '#ed4624', '#e31a1c', '#bd0026', '#800026']
+color_list = ['#ffffe5', '#f7fcb9', '#d9f0a3', '#addd8e', '#82d183', '#78c679', '#41ab5d', '#238443', '#006837', '#004529']
 addColorListToGeoChart('Chart1', layer_name, color_list)
 
 # Define Legend Info for a specific layer of the Geo map with chart name
@@ -40,9 +40,9 @@ addColorListToGeoChart('Chart1', layer_name, color_list)
 #             'scale'(optional) -> Scales of the legend (default - linear)
 #             'grade_list'(optional) - array of number - defined the grades of the scale specifically
 legend_info = {
-'legend_title': 'Average KWTroutedRunoff (m3/s)', 
+'legend_title': 'Scalar SWE (kg m-2)', 
 'legend_position': 'bottomleft',
-'grade_list': [5, 10, 20, 40, 60, 90, 120, 150, 200]
+'scale': 'logarithmic'
 }
 addLegendToGeoChart('Chart1', layer_name, legend_info)
 
@@ -63,7 +63,7 @@ addOptionButtonToGeoChart('Chart1', layer_name, 'Show Average Line Chart', 'Line
 # parma 4 : variable_name - string - name of the variable
 # parma 5 : connecting_id - string - name of the connecting id in geo json (e.g. HRU_ID, COMID)
 # parma 6 : aggregation_type - string - Type of the aggreagation (e.g. average, sum) 
-drawGeoChart('Chart1', layer_name, 'jeoJSONs/bow_river_network.json', variable_name, 'COMID', 'average')
+drawGeoChart('Chart1', layer_name, 'jeoJSONs/bow_river_catchment.json', variable_name, 'HRU_ID', 'average')
 result_array = sorted(result_array, key=lambda x: x['order'], reverse=False)
 print(';##;')
 print(result_array)
--- web_socket_server/web_socket_server.py
+++ web_socket_server/web_socket_server.py
@@ -3,7 +3,7 @@ import json
 import websockets
 import subprocess
 import re
-import os
+import os, random, string
 
 
 SERVER = 'localhost'
@@ -18,7 +18,7 @@ class StoredCommand:
         self.command_list = command_list
 
 
-
+'''
 CLIENTS = set()
 
 async def notify_everyone(message):
@@ -30,15 +30,54 @@ async def register(websocket):
 
 async def unregister(websocket):
     CLIENTS.remove(websocket)
+'''
+
+CLIENTS = []
+
+async def notify_everyone(message):
+    if CLIENTS:
+        await asyncio.wait([client['websocket'].send(message) for client in CLIENTS])
+
+async def notify_client(message, user_id):
+    if CLIENTS:
+        await asyncio.wait([client['websocket'].send(message) for client in CLIENTS if client['user_id'] == user_id])                
+        
+    #await asyncio.wait([user.send(message) for user in CLIENTS])
+
+async def register(websocket, user_id):
+    websocket_obj = {}
+    websocket_obj['user_id'] = user_id
+    websocket_obj['websocket'] = websocket
+    CLIENTS.append(websocket_obj)
+
+async def unregister(websocket):    
+    for client in CLIENTS:
+        if client['websocket'] == websocket:
+            CLIENTS.remove(client)
+            break
+
+def set_user_id(websocket, user_id):
+    for client in CLIENTS:
+        if client['websocket'] == websocket:
+            client['user_id'] = user_id
+            break
+
 
 
 async def python_code_processing(websocket, path):
-    process = subprocess.Popen(['python', '--version'],
-                     stdout=subprocess.PIPE, 
-                     stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    await register(websocket)
-    await websocket.send(json.dumps({ 'output': stdout.decode("utf-8"), 'error': stderr.decode("utf-8"), 'chart':'', 'markup':'' }))
+    #process = subprocess.Popen(['python', '--version'],
+    #                 stdout=subprocess.PIPE, 
+    #                 stderr=subprocess.PIPE)
+    #stdout, stderr = process.communicate()
+
+    user_id = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    #await register(websocket)
+
+    await register(websocket, user_id)
+
+    #await websocket.send(json.dumps({ 'output': stdout.decode("utf-8"), 'error': stderr.decode("utf-8"), 'chart':'', 'markup':'' }))
+    await websocket.send(json.dumps({ 'output': user_id, 'error': '', 'chart':'', 'markup':'', 'message_type': 'connection_establish' }))
+
     try:
         async for message in websocket:
             #message = '{"temp_id":"ABC123","message_type":"request"}'
@@ -47,17 +86,21 @@ async def python_code_processing(websocket, path):
             final_output = ''
             data_code = data.get('code')
             data_return_div = data.get('return_div')
+            user_id = data.get('user_id')
 
             if 'message_type' in data and data['message_type'] == 'request':
                 index = 0
+
+                set_user_id(websocket, user_id)
+
                 for item in list_stored_command:
-                    if item['temp_id'] == data['temp_id']:
+                    if item['temp_id'] == data['temp_id'] and item['user_id'] == user_id:
                         final_output = ';##;' + str(item['commands'])
                         del list_stored_command[index]
                         break
                     index += 1                                        
                         
-                await notify_everyone(json.dumps({ 'output': final_output, 'error': '', 'return_div':data_return_div, 'markup':''}) )
+                await notify_client(json.dumps({ 'output': final_output, 'error': '', 'return_div':data_return_div, 'markup':'', 'message_type': 'stacked_call', 'user_id': user_id}), user_id)
                 continue
 
 
@@ -103,6 +146,7 @@ async def python_code_processing(websocket, path):
                         if 'type' in command and command['type'] == 'new_tab':
                             if 'temp_id' in command:
                                 temp_obj['temp_id'] = command['temp_id']
+                                temp_obj['user_id'] = user_id
                                 final_output += '[' + str(command) + ']' + ';##;'
                                 continue                            
                                 #flag_temp_obj = True
@@ -121,7 +165,7 @@ async def python_code_processing(websocket, path):
             
 
             #await notify_everyone(json.dumps({ 'output': sent_output, 'error': sent_error, 'return_div':data_return_div, 'markup':''}) )
-            await notify_everyone(json.dumps({ 'output': final_output, 'error': sent_error, 'return_div':data_return_div, 'markup':''}) )
+            await notify_client(json.dumps({ 'output': final_output, 'error': sent_error, 'return_div':data_return_div, 'markup':'', 'message_type': 'returned_call', 'user_id': user_id}), user_id)
     except Exception as e:
         await websocket.send(json.dumps({ 'output': '', 'error': 'Internal Error Occured', 'chart':'', 'markup':''}) )
     finally:
